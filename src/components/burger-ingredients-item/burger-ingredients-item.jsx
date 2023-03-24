98d7bb1018baa839b4b21b820681845d9f0f31db
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2023-03-24 15:15:16 +0300

    feat: рефакторит приложение, заменяя классовые компоненты на функциональные.

--- src/components/burger-ingredients-item/burger-ingredients-item.jsx
+++ src/components/burger-ingredients-item/burger-ingredients-item.jsx
@@ -1,54 +1,51 @@
-import React, { Component } from 'react';
 import clsx from 'clsx';
 import styles from './burger-ingredients-item.module.css';
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { ingredientType } from '../../utils/types.js';
 
-class BurgerIngredientsItem extends Component {
-  render() {
-    return (
-      <li
-        className={clsx(styles.ingredients__item)}
-      >
-        <Counter
-          count={1}
-          size="default"
-          extraClass="m-1"
+const BurgerIngredientsItem = (props) => {
+  return (
+    <li
+      className={clsx(styles.ingredients__item)}
+    >
+      <Counter
+        count={1}
+        size="default"
+        extraClass="m-1"
+      />
+      <picture>
+        <source
+          srcSet={props.ingredient.image_mobile}
+          media="(max-width: 480px)"
         />
-        <picture>
-          <source
-            srcSet={this.props.ingredient.image_mobile}
-            media="(max-width: 480px)"
-          />
-          <source
-            srcSet={this.props.ingredient.image_large}
-            media="(min-width: 1400px)"
-          />
-          <img
-            className={clsx(styles.ingredients__image)}
-            alt={this.props.ingredient.name}
-            src={this.props.ingredient.image}
-          />
-        </picture>
-        <div
-          className={clsx(styles.ingredients__price)}
-        >
+        <source
+          srcSet={props.ingredient.image_large}
+          media="(min-width: 1400px)"
+        />
+        <img
+          className={clsx(styles.ingredients__image)}
+          alt={props.ingredient.name}
+          src={props.ingredient.image}
+        />
+      </picture>
+      <div
+        className={clsx(styles.ingredients__price)}
+      >
           <span
             className={clsx('text', 'text_type_digits-default')}
           >
-            {this.props.ingredient.price}
+            {props.ingredient.price}
           </span>
-          <CurrencyIcon type={'primary'}/>
-        </div>
+        <CurrencyIcon type={'primary'}/>
+      </div>
 
-        <h3
-          className={clsx(styles.ingredients__name, 'text', 'text_type_main-default')}
-        >
-          {this.props.ingredient.name}
-        </h3>
-      </li>
-    );
-  }
+      <h3
+        className={clsx(styles.ingredients__name, 'text', 'text_type_main-default')}
+      >
+        {props.ingredient.name}
+      </h3>
+    </li>
+  );
 }
 
 BurgerIngredientsItem.propTypes = {
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,98 +1,89 @@
+import { useState } from 'react';
 import clsx from 'clsx';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import BurgerIngredientsItem from '../burger-ingredients-item/burger-ingredients-item.jsx';
 import styles from './burger-ingredients.module.css';
+import PropTypes from 'prop-types';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import BurgerIngredientsItem from '../burger-ingredients-item/burger-ingredients-item.jsx';
 import { componentType } from '../../utils/types.js';
 
-class BurgerIngredients extends Component {
+const BurgerIngredients = (props) => {
+  const [currentTab, setCurrentTab] = useState('one');
+  const [tabs] = useState([
+    {
+      name: 'Булки',
+      value: 'one',
+      clickFunction: () => changeActiveTab('one')
+    },
+    {
+      name: 'Соусы',
+      value: 'two',
+      clickFunction: () => changeActiveTab('two')
+    },
+    {
+      name: 'Начинки',
+      value: 'three',
+      clickFunction: () => changeActiveTab('three')
+    }
+  ]);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      current: 'one',
-      tabs: [
+  const changeActiveTab = (string) => setCurrentTab(string);
+
+  return (
+    <section>
+      <h1
+        className={clsx('text', 'text_type_main-large', 'mt-10')}
+      >
+        Соберите бургер
+      </h1>
+      <ul
+        className={clsx(styles.tabs_list)}
+      >
         {
-          name: 'Булки',
-          value: 'one',
-          clickFunction: () => this.changeActiveTab('one')
-        },
-        {
-          name: 'Соусы',
-          value: 'two',
-          clickFunction: () => this.changeActiveTab('two')
-        },
-        {
-          name: 'Начинки',
-          value: 'three',
-          clickFunction: () => this.changeActiveTab('three')
+          tabs.map(tab => (
+            <li
+              key={tab.value}
+            >
+              <Tab
+                value={tab.value}
+                active={currentTab === tab.value}
+                onClick={tab.clickFunction}
+              >
+                {tab.name}
+              </Tab>
+            </li>
+          ))
         }
-      ]
-    };
-  }
-
-  changeActiveTab = (string) => {
-    this.setState({ current: string });
-  };
-
-  render() {
-    return (
-      <section>
-        <h1
-          className={clsx('text', 'text_type_main-large', 'mt-10')}
-        >
-          Соберите бургер
-        </h1>
-        <ul
-          className={clsx(styles.tabs_list)}
-        >
-          {
-            this.state.tabs.map(tab => (
-              <li
-                key={tab.value}
+      </ul>
+      <ul
+        className={clsx(styles.ingredients)}
+      >
+        {
+          props.data.map((component, index) => (
+            <li
+              className={clsx(styles.ingredients__column)}
+              key={index}
+            >
+              <h2
+                className={clsx('text', 'text_type_main-medium')}
               >
-                <Tab
-                  value={tab.value}
-                  active={this.state.current === tab.value}
-                  onClick={tab.clickFunction}
-                >
-                  {tab.name}
-                </Tab>
-              </li>
-            ))
-          }
-        </ul>
-        <ul
-          className={clsx(styles.ingredients)}
-        >
-          {
-            this.props.data.map((component, index) => (
-              <li
-                className={clsx(styles.ingredients__column)}
-                key={index}
+                {component.name}
+              </h2>
+              <ul className={clsx(styles.ingredients__list)}
               >
-                <h2
-                  className={clsx('text', 'text_type_main-medium')}
-                >
-                  {component.name}
-                </h2>
-                <ul className={clsx(styles.ingredients__list)}
-                >
-                  {
-                    component.items.map(item => (
-                      <BurgerIngredientsItem ingredient={item} key={item._id}/>
-                    ))
-                  }
-                </ul>
-              </li>
-            ))
-          }
-        </ul>
-      </section>
-    );
-  }
-}
+                {
+                  component.items.map(item => (
+                    <BurgerIngredientsItem ingredient={item} key={item._id}/>
+                  ))
+                }
+              </ul>
+            </li>
+          ))
+        }
+      </ul>
+    </section>
+  );
+};
+
 
 BurgerIngredients.propTypes = {
   data: PropTypes.arrayOf(componentType),
