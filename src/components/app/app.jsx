caa57656d1e5ab22738c40ee5cb1d5764699086f
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2023-04-20 12:24:57 +0300

    feat: правит ошибки выявленные на ревью, добавляет список ингредиентов в контекст.
    Исправляет:
    
    - В App.jsx необходимо передавать список ингредиентов через контекст, а не через пропсы.
    - Не стоит оборачивать в контекст элементы, которые не будут его использовать. Иначе они будут перерендериться каждый раз как меняется значение контекста, даже если они его не используют.
    - В post запрос на сервер необходимо добавить в хедер свойство Content-type - иначе сервер может неправильно обработать - добавляет комментарий о том как добавляется Content-type в приложении.
    - Оборачивает содержимое контекста в useMemo.
    - Исправляет запись "||" в PropTypes компонента OrderDetails.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -12,35 +12,49 @@ import OrderDetails from '../order-details/order-details.jsx';
 import { useIngredients } from '../../hooks/useIngredients.js';
 import { useModal } from '../../hooks/useModal.js';
 import { CartContext } from '../../context/cartContext.js';
+import { IngredientsContext } from '../../context/ingredientsContext.js';
 import { useCart } from '../../hooks/useCart.js';
+import { useMemo } from 'react';
 
 const App = () => {
   const { ingredients, serverData, error, loading } = useIngredients();
   const { cart, dispatch, state } = useCart(ingredients);
   const { detailedIngredient, isDetailedOrderOpened, isModalOpen, closeModal, openModal } = useModal();
 
+  const ingredientsContextValue = useMemo(() => {
+    return { ingredients };
+  }, [ingredients]);
+
+  const cartContextValue = useMemo(() => {
+    return { cart, dispatch, state };
+  }, [cart, dispatch, state]);
+
   return (
-    <CartContext.Provider value={{ cart, dispatch, state }}>
+    <>
       <Header/>
       <main className={clsx(styles.main, 'pb-10')}>
         {
           !loading && serverData.length > 0
             ?
-            <div className={clsx(styles.main_container)}>
-              <BurgerIngredients
-                ingredients={ingredients}
-                openModal={openModal}
-              />
-              <BurgerConstructor
-                openModal={openModal}
-              />
-            </div>
+            <IngredientsContext.Provider value={ingredientsContextValue}>
+              <CartContext.Provider value={cartContextValue}>
+                <div className={clsx(styles.main_container)}>
+                  <BurgerIngredients
+                    openModal={openModal}
+                  />
+                  <BurgerConstructor
+                    openModal={openModal}
+                  />
+                </div>
+              </CartContext.Provider>
+            </IngredientsContext.Provider>
             : <Loader loading={loading}/>
         }
         {
           error && <h1>Ошибка</h1>
         }
       </main>
+      ;
 
       <Modal
         isModalOpen={isModalOpen}
@@ -56,9 +70,10 @@ const App = () => {
           isDetailedOrderOpened &&
           <OrderDetails orderNumber={cart.orderNumber}/>
         }
-      </Modal>
-    </CartContext.Provider>
-  );
+      </Modal>;
+    </>
+  )
+    ;
 };
 
 export default App;
